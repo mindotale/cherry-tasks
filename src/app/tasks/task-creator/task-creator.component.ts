c62eb43ce68c8ecb962ff179ddd1dceb046f731c
Volodymyr Domin <mindotale@gmail.com>
2025-01-02 22:23:12 +0200

    feat: add TaskCreator component with initial template

---
+++ src/app/tasks/task-creator/task-creator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-creator',
+  imports: [],
+  templateUrl: './task-creator.component.html',
+  styleUrl: './task-creator.component.css'
+})
+export class TaskCreatorComponent {
+
+}
